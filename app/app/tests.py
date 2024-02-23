c95cde1a543593d32d44591e174047d875adfe62
Rakib <rakib@Rakibs-MacBook-Pro.local>
2024-02-23 23:22:48 +0600

    unit testing

---
+++ app/app/tests.py
@@ -0,0 +1,21 @@
+"""
+simple test  
+"""
+
+from django.test import SimpleTestCase
+
+
+from app import calc
+
+class CalcTest(SimpleTestCase):
+  """
+  test the calc module
+  """
+  
+  def test_add_numbers(self):
+    """
+    test adding numbers together
+    """
+    
+    res = calc.add(5,6)
+    self.assertEqual(res,11)
